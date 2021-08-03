f40a9c9a6a0077d9b4eafe1a246eaec133bb59bc
Paul Vaneveld <paul@liters.nl>
2021-08-03 18:18:24 +0200

    finished :)

--- classes/Splash.ts
+++ classes/Splash.ts
@@ -6,32 +6,54 @@ export class Splash {
     private angle: number;
     private p5: p5;
     private speed: number;
-    private iterations = 1;
+    private currentIteration = 1;
+    private totalIterations = 30;
+    private speedRange: [number, number] = [1, 3];
+    private sizeRange: [number, number] = [5, 20];
 
     constructor(p5: p5) {
         this.p5 = p5;
         this.x = p5.random(0, p5.windowWidth);
         this.y = p5.windowHeight;
         this.angle = Math.floor(p5.random(30, 160));
-        this.speed = p5.random(1, 3);
+        this.speed = p5.random(...this.speedRange);
     }
 
     public splash() {
-        if (this.iterations < 30) {
-            this.x = this.x + this.p5.cos((this.p5.PI * this.angle) / 180) * this.speed;
-            this.y = this.y + Math.sin((this.p5.PI * this.angle) / 180) * this.speed * -1;
-            this.iterations++;
+        if (this.currentIteration < this.totalIterations) {
+            this.updatePosition();
+            this.currentIteration++;
         } else {
-            this.x = this.p5.random(this.p5.windowWidth);
-            this.y = this.p5.windowHeight;
-            this.speed = this.p5.random(1, 3);
-            this.iterations = 1;
+            this.resetSplash();
         }
     }
 
+    private updatePosition() {
+        // move at an angle
+        this.x = this.x + this.p5.cos((this.p5.PI * this.angle) / 180) * this.speed;
+        this.y =
+            this.y + Math.sin((this.p5.PI * this.angle) / 180) * this.speed * -1;
+    }
+
+    private resetSplash() {
+        this.x = this.p5.random(this.p5.windowWidth);
+        this.y = this.p5.windowHeight;
+        this.speed = this.p5.random(...this.speedRange);
+        this.currentIteration = 1;
+    }
+
     public show() {
+        // the lower the speed the smaller the splash
+        const size = this.p5.map(this.speed, ...this.speedRange, ...this.sizeRange);
+
+        // set purple color
         this.p5.fill(148, 43, 226);
         this.p5.noStroke();
-        this.p5.circle(this.x, this.y - 20 / 2, 20 - (20 / 30) * this.iterations);
+
+        this.p5.circle(
+            this.x,
+            this.y - size / 2, // start halfway below screen
+            size - (size / this.totalIterations) * this.currentIteration // get smaller gradually
+        );
     }
 }
--- index.ts
+++ index.ts
@@ -7,54 +7,69 @@ const sketch = (p5: p5) => {
     let splashes: Splash[] = [];
 
     p5.setup = async () => {
-        p5.createCanvas(p5.windowWidth, p5.windowHeight);
-        drops = getDrops(p5);
-        await getSplashes(p5, splashes);
+        await init();
     };
+
+    p5.windowResized = async () => {
+        await init();
+    }
 
     p5.draw = async () => {
         p5.background(230, 230, 250);
         initDrops(drops);
         initSplashes(splashes);
     };
-};
 
-const getDrops = (p5: p5) => {
-    let drops: Drop[] = [];
-    for (let index = 0; index < 500; index++) {
-        drops.push(new Drop(p5));
+    const init = async () => {
+        p5.createCanvas(p5.windowWidth, p5.windowHeight);
+        drops = getDrops(p5);
+
+        // wait for the drops to hit the ground :)
+        await sleep(1000);
+
+        // go splash!
+        splashes = [];
+        await getSplashes(p5, splashes);
     }
 
-    return drops;
-};
 
-const initDrops = (drops: Drop[]) => {
-    drops.forEach((drop) => {
-        drop.show();
-        drop.fall();
-    });
-};
+    const getDrops = (p5: p5) => {
+        let drops: Drop[] = [];
+        for (let index = 0; index < p5.windowWidth / 2; index++) {
+            drops.push(new Drop(p5));
+        }
 
-const getSplashes = async (p5: p5, splashes: Splash[]) => {
-    for (let index = 0; index < p5.windowWidth / 20; index++) {
+        return drops;
+    };
 
-        await sleep(p5.random(10, 30));
-        splashes.push(new Splash(p5));
+    const initDrops = (drops: Drop[]) => {
+        drops.forEach((drop) => {
+            drop.show();
+            drop.fall();
+        });
+    };
+
+    const getSplashes = async (p5: p5, splashes: Splash[]) => {
+        for (let index = 0; index < p5.windowWidth / 20; index++) {
+            await sleep(p5.random(10, 30));
+            splashes.push(new Splash(p5));
+        }
     }
-}
 
-const initSplashes = (splashes: Splash[]) => {
-    splashes.forEach((splash) => {
-        splash.show();
-        splash.splash();
-    });
+    const initSplashes = (splashes: Splash[]) => {
+        splashes.forEach((splash) => {
+            splash.show();
+            splash.splash();
+        });
+    };
+
+
+    const sleep = (millisecondsDuration: number) => {
+        return new Promise((resolve) => {
+            setTimeout(resolve, millisecondsDuration);
+        });
+    }
 };
 
 
-const sleep = (millisecondsDuration: number) => {
-    return new Promise((resolve) => {
-        setTimeout(resolve, millisecondsDuration);
-    });
-}
-
-const sketchInstance = new p5(sketch);
+new p5(sketch);
